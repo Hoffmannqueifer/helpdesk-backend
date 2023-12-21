da8f4c6c517f4601a28da51127dbca68e539e4a2
hoffmannqueifer <hoffmannqueifer23@gmail.com>
2023-12-21 14:54:12 -0300

    endpoint findById para tecnico

---
+++ src/main/java/com/hoffmann/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,25 @@
+package com.hoffmann.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoffmann.helpdesk.domain.Tecnico;
+import com.hoffmann.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService service;
+
+	@GetMapping(value = "{id}")
+	public ResponseEntity<Tecnico> findBuID(@PathVariable Integer id){
+		Tecnico obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
