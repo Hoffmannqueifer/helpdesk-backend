640139974be6823ff399d03102752bef4f96ecef
hoffmannqueifer <hoffmannqueifer23@gmail.com>
2023-12-22 10:05:04 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/hoffmann/helpdesk/services/TecnicoService.java
+++ src/main/java/com/hoffmann/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.hoffmann.helpdesk.domain.Tecnico;
 import com.hoffmann.helpdesk.repositories.TecnicoRepository;
+import com.hoffmann.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Tecnico não encontrado: ID: "+ id));
 	}
 }
