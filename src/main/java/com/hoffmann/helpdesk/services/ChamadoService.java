e70409b8923febf018d447058b128efcb808bfa6
hoffmannqueifer <hoffmannqueifer23@gmail.com>
2023-12-28 13:59:17 -0300

    endpoint findAll para chamados

--- src/main/java/com/hoffmann/helpdesk/services/ChamadoService.java
+++ src/main/java/com/hoffmann/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.hoffmann.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ public class ChamadoService {
 	
 	public Chamado findByID(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" + id));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! ID:" + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 }
