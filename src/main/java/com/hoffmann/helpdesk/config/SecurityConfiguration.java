2fd774eb2f4da30e02494493110fef9d3f6194af
hoffmannqueifer <hoffmannqueifer23@gmail.com>
2024-01-04 11:12:06 -0300

    Autenticacao com jwt

--- src/main/java/com/hoffmann/helpdesk/config/SecurityConfiguration.java
+++ src/main/java/com/hoffmann/helpdesk/config/SecurityConfiguration.java
@@ -6,14 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.hoffmann.helpdesk.security.JWTAuthenticationFilter;
+import com.hoffmann.helpdesk.security.JWTUtil;
 
 
 
@@ -25,19 +33,37 @@ public class SecurityConfiguration {
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
+	private final AuthenticationConfiguration authenticationConfiguration;
+	
+	@Autowired
+    public SecurityConfiguration(AuthenticationConfiguration configuration) {
+        this.authenticationConfiguration = configuration;
+    }
+	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
 		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
+		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		return http.csrf(csrf -> csrf.disable()).
 				sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
 				authorizeHttpRequests(authorize -> authorize.requestMatchers(PUBLIC_MATCHERS).permitAll().
 						anyRequest().authenticated()).build();
 		
     }
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
@@ -52,4 +78,9 @@ public class SecurityConfiguration {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+    	return authenticationConfiguration.getAuthenticationManager();
+    }
 }
